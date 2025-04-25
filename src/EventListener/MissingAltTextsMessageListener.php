da81eded2eacb615c35d20d4c003f6f34b1b63cf
Lukas Bableck <lukas@bableck.dev>
2025-04-25 07:48:39 +0200

    show system message only for users with permission for alt editor

--- src/EventListener/MissingAltTextsMessageListener.php
+++ src/EventListener/MissingAltTextsMessageListener.php
@@ -5,13 +5,18 @@ use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
 use Contao\System;
 use Lukasbableck\ContaoAltEditorBundle\Classes\AltEditor;
 use Lukasbableck\ContaoAltEditorBundle\Controller\AltEditorBackendController;
+use Symfony\Bundle\SecurityBundle\Security;
 
 #[AsHook('getSystemMessages')]
 class MissingAltTextsMessageListener {
-	public function __construct(private readonly AltEditor $altEditor) {
+	public function __construct(private readonly AltEditor $altEditor, private readonly Security $security) {
 	}
 
 	public function __invoke(): string {
+		if (!$this->security->isGranted('ROLE_ADMIN') && !$this->security->isGranted('contao_user.alteditor')) {
+			return '';
+		}
+
 		if (!$this->altEditor->areAltTextsMissing()) {
 			return '';
 		}
